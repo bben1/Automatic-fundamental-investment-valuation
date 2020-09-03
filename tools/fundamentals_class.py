73a60579854c89f5c5b46d775b700efe4bbabbfa
bben1 <70587066+bben1@users.noreply.github.com>
2020-09-03 17:06:44 +0100

    Update fundamentals_class.py

--- tools/fundamentals_class.py
+++ tools/fundamentals_class.py
@@ -1,15 +1,3 @@
-import requests
-import pandas as pd 
-import numpy as np
-import datetime
-
-import matplotlib.pyplot as plot
-import seaborn as sns
-
-import pandas_datareader.data as web
-import datetime
-from pandas.util.testing import assert_frame_equal
-
 class Fundamentals:
     
     def __init__(self, income_statement, balance_sheet_statement, balance_sheet_statement_quarterly, cash_flow_statement, enterprise_value, financial_ratios, company_ticker, forecasting_period, api_key):
